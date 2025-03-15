ebedbaf59c91abb687cd5f5cdda156a622e34ade
Dudermont <dkprogramer@gmail.com>
2025-03-15 23:12:36 +0300

    product_list and pagination

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 
@@ -8,6 +9,7 @@ def popular_list(request):
                   'main/index/index.html',
                   {'products': products})
 
+
 def product_detail(request, slug):
     product = get_object_or_404(Product,
                                 slug=slug,
@@ -15,3 +17,24 @@ def product_detail(request, slug):
     return render(request,
                   'main/product/detail.html',
                   {'product': product})
+
+
+def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 10)
+    current_page = paginator.page(int(page))
+    if category_slug:
+        category = get_object_or_404(Category,
+                                     slug=category_slug)
+        products = products.filter(category=category)
+        paginator = Paginator(products, 10)
+        current_page = paginator.page(int(page))
+    return render(request,
+                  'main/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': current_page,
+                   'slug_url': category_slug})
