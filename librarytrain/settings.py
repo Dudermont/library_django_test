c4f12befe97de45377ca7dfc645c30a58af215bf
Dudermont <dkprogramer@gmail.com>
2025-03-16 14:23:36 +0300

    add cart

--- librarytrain/settings.py
+++ librarytrain/settings.py
@@ -21,9 +21,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 # apps
 INSTALLED_APPS += [
     'main',
+    'cart',
 ]
 
 
@@ -50,6 +52,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart'
             ],
         },
     },
@@ -112,3 +115,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
